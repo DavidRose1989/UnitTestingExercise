82a8ef2f69e1f8af0ffdf5f8ade1cbf9e43b7bb5
DavidRose <DavidRose331@Gmail.com>
2023-02-27 12:48:38 -0600

    initial

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.WebSockets;
 using Xunit;
 
 namespace UnitTestingExercise.Tests
@@ -13,53 +14,75 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
+            var test = new Calculator();
             
 
             //Act
                 // call the Add method that is located in the Calculator class
                 // and store its result in a variable named actual
 
+                 var actual = test.Add(num1, num2, num3);
+
             //Assert
-                //Assert.Equal(expected, actual);
+                 Assert.Equal(expected, actual);
+        }
+
+        private object Add(int num1, int num2, int num3)
+        {
+            throw new NotImplementedException();
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(7,6,1)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
 
+            var test = new Calculator();
+
             //Act
 
-            //Assert
+            var actual = test.Subtract(minuend, subtrhend);
 
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(7,7,49)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
 
+            var multTest = new Calculator();
+
             //Act
 
-            //Assert
+            var actual = multTest.Multiply(num1, num2);
 
+            //Assert
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(100,10,10)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
+            
+            var divTest = new Calculator();
 
             //Act
 
+            var actual = divTest.Divide(num1, num2);
+
             //Assert
+
+            Assert.Equal(expected, actual);
 
         }
 
--- UnitTestingExercise/Calculator.cs
+++ UnitTestingExercise/Calculator.cs
@@ -3,9 +3,11 @@ namespace UnitTestingExercise
 {
     public class Calculator
     {
-        // Step 1: Add a reference to your UnitTestingExercise project:
+        // done - Step 1: Add a reference to your UnitTestingExercise project:
 
-        // Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
+
+
+        // done - Right-click on the 'Dependencies' folder in your UnitTestingExercise.Tests project
         // and add your reference to the UnitTestingExercise project
 
 
@@ -18,28 +20,44 @@ namespace UnitTestingExercise
         }
 
 
-        // Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
+        // done - Step 3: Navigate to the UnitTests.cs file and complete the AddTest unit test method 
 
 
         // Step 4: 
         // Create a Subtract method that accepts 2 integers
         // Keep track of which number is getting passed as minuend and subtrahend
 
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
-        // Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
+
+        // done - Step 5: Navigate to the UnitTests.cs file and complete the SubtractTest unit test method
 
 
         // Step 6: 
         // Create a Multiply method that passes 2 integers
 
+            public int Multiply(int product, int product2)
+            {
 
-        // Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
+            return product * product2;
+
+            }
+
+        // done - Step 7: Navigate to the UnitTests.cs file and complete the MultiplyTest unit test method 
 
 
         // Step 8: 
         // Create a Divide method that passes 2 integers
 
-        // Step 9: Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
+        public int Divide(int num1, int num2)
+        { 
+        return num1 / num2; 
+        }
+
+        // Step 9: done Navigate to the UnitTests.cs file and complete the DivideTest unit test method 
 
 
         //*****NOW RUN ALL OF YOUR TESTS*********//
